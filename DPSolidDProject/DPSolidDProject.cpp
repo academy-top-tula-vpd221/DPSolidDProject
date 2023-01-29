ce9950cb193d7d5e24a9888b2c6ed1b29594ccdc
Ефимов Максим <efimov_m@itstep.academy>
2023-01-29 17:55:37 +0300

    Добавьте файлы проекта.

---
+++ DPSolidDProject/DPSolidDProject.cpp
@@ -0,0 +1,55 @@
+﻿// DPSolidDProject.cpp 
+#include <iostream>
+
+using namespace std;
+
+class IPrinter
+{
+public:
+    virtual void Print(string text) = 0;
+};
+
+class Book
+{
+    string text;
+    IPrinter* printer;
+    
+public:
+    Book(IPrinter* printer = nullptr) : printer{ printer } {}
+
+    string& Text() { return text; }
+    IPrinter*& Printer() { return printer; }
+
+    void Print()
+    {
+        printer->Print(text);
+    }
+};
+
+class ConsolePrinter : public IPrinter
+{
+public:
+    void Print(string text) override
+    {
+        cout << "text: " << text << "\n";
+    }
+};
+
+class HtmlPrinter : public IPrinter 
+{
+public:
+    void Print(string text) override
+    {
+        cout << "<html>" << text << "</html>\n";
+    }
+};
+
+
+
+int main()
+{
+    Book book(new HtmlPrinter());
+    book.Text() = "Hello world";
+
+    book.Print();
+}
